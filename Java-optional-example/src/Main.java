28187d533e7fede0e6a22f4b4920f69cdfc5dbba
HP <pravinltupe4399@gmail.com>
2023-01-16 19:40:10 +0530

    Adding Java-Example module

---
+++ Java-optional-example/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final String name = "Pravin";
+
+        if (name != null && !name.isEmpty() && !(name.length() > 3)) {
+            System.out.println(" Length of String in greater than  3 ");
+        } else {
+            System.out.println("String of either null or Empty or less than 3 ");
+        }
+        Optional<String> name2 =Optional.ofNullable(name);
+         if ( !name.isEmpty() && name2.get().length() > 3){
+             System.out.println("Length of String is greater than than 3");
+         } else {
+             System.out.println("String is either null or empty or less than 3 ");
+         }
+    }
+
+}
+
+
